9ae0d533d6302e5ceb139a2ddfd9a5e1138aaec5
Kyle Rego <regoky@outlook.com>
2024-09-24 22:02:54 -0400

    More CSS reworking

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,13 +6,11 @@ export default function Hero() {
     return <section id="hero" className="d-flex justify-content-center align-items-center">
                 <div className="section-card">
                         <div className="d-flex flex-column align-items-center column-gap-1">
-                            <div className="hero-card d-flex justify-content-center column-gap-3 align-items-center">
-                                <div className="hero-card-image">
-                                    <img src="me.png" alt="Me" />
-                                </div>
+                            <div className="hero-card d-flex flex-wrap-reverse justify-content-center column-gap-3 align-items-center">
+
                                 <div>
                                     <div className="mb-2 hero-card-heading">
-                                        Kyle ("") Rego 
+                                        Kyle Rego
                                     </div>
                                     <div className="mb-2 hero-card-text">
                                         Software Developer (Web)
@@ -21,14 +19,18 @@ export default function Hero() {
                                         <a href="mailto:regoky@outlook.com">Email</a>
                                         <a href="https://www.linkedin.com/in/kyle-rego-117b25139/">LinkedIn</a>
                                     </div>
-                                </div>         
+                                </div>
+
+                                <div className="hero-card-image">
+                                    <img src="me.png" alt="Me" />
+                                </div> 
                             </div>
 
                             <p className="text-center font-20px" style={{maxWidth: "720px"}}>
-                                Objective: to be hired in a software developer, support engineer, or other technical position to grow as an IT professional.
+                                Portfolio Objective: to be hired in a software developer, support engineer, or other technical position to grow as an IT professional.
                             </p>
 
-                            <div className="d-flex flex-wrap align-items-start justify-content-around column-gap-3 row-gap-1">
+                            <div className="d-flex flex-wrap align-items-start justify-content-around column-gap-3 row-gap-1 mb-4">
                                 <List heading={"Languages"} items={["Java (learning)", "C#", "JavaScript", "Ruby"]} />
 
                                 <List heading={"Frameworks and libraries"} items={["Spring Boot (learning)", "ASP.NET Core", "React", "Blazor", "Ruby on Rails"]} />
