7810134e7a45f219ac50c9aea6b9566b4f8ad635
Kyle Rego <regoky@outlook.com>
2024-12-25 11:47:39 -0800

    Fix layout issue

--- src/Gob.jsx
+++ src/Gob.jsx
@@ -3,14 +3,16 @@ import DemoLink from "./components/DemoLink";
 export default function Gob() {
     return <section id="gob-guides" className="d-flex justify-content-center align-items-center">
                 <div className="section-card">
-                    <h2 className="font-42px">Lilgobslayerguides.NET</h2>
-
-                    <div className="mt-2">
+                    <div className="mb-4 d-flex flex-wrap justify-content-center align-items-center column-gap-3 row-gap-1">
+                        <h2 className="font-42px" style={{wordBreak: "break-all"}}>Lilgobslayerguides.NET</h2>
+                        
                         <DemoLink href="https://lilgobslayerguides.net"
-                                                                text="Gob Guides" />
+                                            text="Gob Guides" />
+                        
                     </div>
+
                     
                 </div>
 
-            </section>;
+        </section>;
 }
