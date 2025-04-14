4671d0310cde6b8d1b3604642e183dc768921148
Kyle Rego <regoky@outlook.com>
2025-04-13 23:47:08 -0700

    Work on hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,41 +1,82 @@
 export default function Hero() {
+
+    const resumes = [
+        ["KR Tech Resume", "https://kylerego.github.io/assets/kylerego_resume.pdf"],
+        ["KR Jobs Resume", "https://kylerego.github.io/assets/kyle_jobs_resume.pdf"]
+    ]
+
     return <section id="hero" className="d-flex justify-content-center align-items-center">
                 <div className="card">
                     <div className="d-flex flex-wrap justify-content-center row-gap-1 align-items-center column-gap-3">
-                        <div className="d-flex flex-column" style={{rowGap: "0.5rem"}}>
+                        <div className="d-flex flex-column align-items-center" style={{rowGap: "0.5rem"}}>
                             <h1 className="fs-1">
-                                Kyle Rego
+                                Kyle Egg Rain
                             </h1>
                             <div>
                                 Tacoma, Washington, USA
                             </div>
-                            <a href="https://kylerego.github.io/assets/kylerego_resume.pdf">
-                                Software Developer Resume
-                            </a>
-                            <a href="https://kylerego.github.io/assets/kyle_jobs_resume.pdf">
-                                Jobs Resume
-                            </a>
+                            <div className="d-flex column-gap-1 align-items-center">
+                                {resumes.map(resume => < ResumeCard href={resume[1]} text={resume[0]} />)}
+                            </div>
+                            
                         </div>
 
-                        <figure style={{margin: 0}}>
-                            <div className="d-flex column-gap-1 align-items-center">
-                                <img src="me_at_ups_cornerstone.jpg" alt="Me at UPS"
-                                style={{width: "150px", height: "150px", borderRadius: "50%"}} />
-                                <figcaption>
-                                    Me at UPS
+                        <figure style={{ margin: 0 }}>
+                            <div className="d-flex flex-column align-items-center" style={{ position: "relative" }}>
+                                <img
+                                    src="me_at_ups_cornerstone.jpg"
+                                    alt="Me at UPS"
+                                    style={{
+                                        width: "150px",
+                                        height: "150px",
+                                        borderRadius: "50%",
+                                        flexShrink: 0
+                                    }}
+                                />
+                                <figcaption style={{
+                                    transform: "rotate(4deg)",
+                                    transformOrigin: "left center"
+                                }}>
+                                    Me in UPS (Nov, 2024)
                                 </figcaption>
                             </div>
                         </figure>
+
                     </div>
 
                     <p>
-                        <span style={{ fontSize: "28px" }}>Hi there!</span> I'm Kyle Rego, a self-taught software developer on a quest to create great software. I like problem-solving, learning about new things, and contributing to the community.
-                    </p>
+                        I'm Kyle Egg Rain (formerly Kyle "Real Good" Rego). 
+              
+                        In these uncertain times, I am working for UPS and Domino's as a loader/unloader and delivery driver, respectively. 
+             
+                        I've positioned myself near Seattle, Washington as a step toward my long term goal of a career in software development.
 
-                    <p>
-                        Right now, I'm working at UPS while exploring different job opportunities to make ends meet. I'm open to various roles—whether in IT, technical support, or other fields—as I work toward returning to software development in the long run. If you're hiring in the Tacoma area, feel free to <a href="mailto:regoky@outlook.com">reach out</a>—I'd love to help your team!
+                        To that end, I am considering starting an MS in Computer Science soon, developing several software projects, and system administrating an Azure Linux machine to showcase them (excluding the blog).
                     </p>
+             
 
                 </div>
         </section>;
+}
+
+function ResumeCard({href, text}) {
+    return (
+        <a href={href} style={{
+            height: "86px",
+            width: "78px",
+            padding: "16px",
+            backgroundColor: "#fff",
+            boxShadow: "1px 1px 1px 1px rgb(0, 0, 0)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            fontSize: "15px",
+            transform: "rotate(-1deg)",
+            transformOrigin: "left center"
+        }}>
+
+            {text}
+        </a>
+    );
 }
