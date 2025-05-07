1e86f8ff265d47125962e4ce6fb7d02b27f37353
Kyle Rego <regoky@outlook.com>
2025-05-07 16:10:18 -0700

    Continue moving stuff

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,61 +6,40 @@ export default function Hero() {
     ]
 
     return <section id="hero" className="d-flex justify-content-center align-items-center">
-                <div className="card">
-                    
+                <div className="">
+                    <div className="card">
+                        <div className="d-flex justify-content-center align-items-center">
+                            <div className="d-flex flex-wrap column-gap-1 row-gap-1 align-items-center justify-content-center">
+                                <div className="d-flex column-gap-1 align-items-end">
+                                    <ResumeCard href={resumes[0][1]} text={resumes[0][0]} />
 
-                    <div className="">
-                        <div className="d-flex column-gap-1 align-items-end">
-                        <div>
-                            <div className="d-flex justify-content-center align-items-center">
-                                <div className="d-flex flex-wrap column-gap-1 row-gap-1 align-items-center justify-content-center">
-                                    <div className="d-flex column-gap-1">
-                                        < ResumeCard href={resumes[0][1]} text={resumes[0][0]} />
+                                    <HeroHeader />
 
-                                        <HeroHeader />
-
-                                        < ResumeCard href={resumes[1][1]} text={resumes[1][0]} />
-                                    </div>
-
+                                    <ResumeCard href={resumes[1][1]} text={resumes[1][0]} />
                                 </div>
+
                             </div>
-                            <p> 
-                                Hi, I am Kyle Rain, and this website is my software developer portfolio. 
-                            </p>
+                        </div>
+                        <p> 
+                            Hi, I am Kyle Rain, and this website is my software developer portfolio. 
+                        </p>
 
-                            <p>
-                                I work for UPS and Domino's as a loader/unloader and delivery driver, respectively, while I work toward a software developer role (onsite or hybrid with a C# stack preferably).  
-                            </p>
-                            <p>
-                                In my free time, I enjoy cooking, hitting the gym, writing code, and learning stuff.
-                            </p>
-                        </div>
-                        <figure style={{ margin: 0, width: "205px" }} className="flex-1">
-                                <div className="d-flex align-items-center" style={{ position: "relative" }}>
-                                    <img
-                                        src="me_at_ups_cornerstone.jpg"
-                                        alt="Me at UPS"
-                                        style={{
-                                            width: "120px",
-                                            height: "120px",
-                                            borderRadius: "50%",
-                                            flexShrink: 0
-                                        }}
-                                    />
-                                    <figcaption style={{
-                                        transform: "rotate(4deg)",
-                                        transformOrigin: "left center"
-                                    }}>
-                                        Me in UPS (Nov, 2024)
-                                    </figcaption>
-                                </div>
-                        </figure>
-                        </div>
-                        
+                        <p>
+                            I work for UPS and Domino's as a loader/unloader and delivery driver, respectively, while I work toward a software developer role (onsite or hybrid with a C# stack preferably).  
+                        </p>
+                        <p>
+                            In my free time, I enjoy cooking, hitting the gym, writing code, and learning stuff.
+                        </p>
+                    </div>
+
+                    <div className="d-flex justify-content-end">
+                        <UPSPicture />
                     </div>
                     
                 </div>
-        </section>;
+
+                
+            </section>;
 }
 
 function ResumeCard({href, text}) {
@@ -94,4 +73,27 @@ function HeroHeader() {
                                     Tacoma, Washington, USA
                                 </div>                       
                             </div>;
+}
+
+function UPSPicture() {
+    return <figure style={{ margin: 0, width: "205px" }} className="flex-1">
+                                <div className="d-flex align-items-center" style={{ position: "relative" }}>
+                                    <img
+                                        src="me_at_ups_cornerstone.jpg"
+                                        alt="Me at UPS"
+                                        style={{
+                                            width: "120px",
+                                            height: "120px",
+                                            borderRadius: "50%",
+                                            flexShrink: 0
+                                        }}
+                                    />
+                                    <figcaption style={{
+                                        transform: "rotate(4deg)",
+                                        transformOrigin: "left center"
+                                    }}>
+                                        Me in UPS (Nov, 2024)
+                                    </figcaption>
+                                </div>
+                        </figure>
 }
