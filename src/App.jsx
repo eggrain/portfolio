f71ea68126c63430d33931e3220f64fe54ec76df
Kyle Rego <regoky@outlook.com>
2026-06-27 12:19:41 -0700

    Remove menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import Hero from "./Hero";
-import AnkiBooks from "./AnkiBooks";
-import Larder from "./Larder";
-import Menu from "./Menu";
-import Blog from "./Blog";
-import Oldschool from "./Oldschool";
-import Todolist from "./Todolist";
-import Health from "./Health";
 
 import "./css/App.css";
-
 import "./css/utility.css";
 import "./css/flex.css";
 import "./css/fonts.css";
 import "./css/backgrounds.css";
-import Hots from "./Hots";
 
 export default function App() {
     return <>
-        <Menu />
-
         <div className="App" style={{ width: "100%", maxWidth: "100vw" }}>
             <Hero />
         </div>
